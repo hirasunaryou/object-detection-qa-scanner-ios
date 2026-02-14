89c6f0a1d72e387a2631192eab59bb84d20f8154
Kenichi Takei <hirasunaryou@gmail.com>
2026-02-14 15:52:50 +0900

    initial empty repo

---
+++ ObjectDetectionQAScanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ObjectDetectionQAScanner
+//
+//  Created by Kenichi Takei on 2026/02/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ObjectDetectionQAScanner/ObjectDetectionQAScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectDetectionQAScannerApp.swift
+//  ObjectDetectionQAScanner
+//
+//  Created by Kenichi Takei on 2026/02/14.
+//
+
+import SwiftUI
+
+@main
+struct ObjectDetectionQAScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
